2706c66b8c09f58849d176355d3df92e7cf2afdf
CathyChian <chiancathy@gmail.com>
2020-10-10 15:52:40 -0400

    changed states to cities and added date input

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$("#options").on('change', function() {
-    $("#results").html(this.value);
+$("#submit").on('click', function() {
+    $("#results").html("Dropdown value: " + $("#cities").val());
+});
+
+var day, month, year;
+
+$('#submit').on('click', function() {
+    var date = $('#date').val().split("-");
+    day = date[2];
+    month = date[1];
+    year = date[0];
+    $("#results").append("<br>Date: " + day + ", Month: " + month + ", Year: "+ year);
 });
