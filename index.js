93cd1b358dea75bd2e0446119ca3542e15e509ab
CathyChian <chiancathy@gmail.com>
2020-10-10 21:52:42 -0400

    select instead of choose

--- index.js
+++ index.js
@@ -42,12 +42,12 @@ $('#submit').on('click', function() {
 
     // If a city isn't chosen
     if ($("#cities").val() === "") {
-        $("#results").html("Please choose a city.");
+        $("#results").html("Please select a city.");
     }
 
     // If a date is chosen before 10/11/2020
     if (date[0] < 2020 || (date[0] == 2020 && date[1] < 10) ||
         (date[0] == 2020 && date[1] == 10 && date[2] < 11)) {
-        $("#results").html("Please choose a date in the future.");
+        $("#results").html("Please select a date in the future.");
     }
 });
